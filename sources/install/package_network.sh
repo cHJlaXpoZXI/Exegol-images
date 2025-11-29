2e73dbc70c6fc3a077c18f80d405600cd7904237
Shutdown <40902872+ShutdownRepo@users.noreply.github.com>
2025-11-29 18:40:02 +0100

    changing order of nmap for history priority

--- sources/install/package_network.sh
+++ sources/install/package_network.sh
@@ -328,8 +328,8 @@ function package_network() {
     install_network_apt_tools
     install_proxychains             # Network tool
     install_remmina                 # Remote desktop client
-    install_nmap                    # Port scanner
     install_nmap-parse-output       # Parse nmap XML files
+    install_nmap                    # Port scanner
     install_udpx
     install_autorecon               # External recon tool
     install_dnschef                 # Python DNS server
