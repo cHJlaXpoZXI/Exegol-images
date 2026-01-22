86fa7d68929865901f59fd97e8cdcfb6c66e8e8b
Shutdown <40902872+ShutdownRepo@users.noreply.github.com>
2026-01-22 15:03:48 +0100

    usng python tldr client instead of apt package

--- sources/install/package_base.sh
+++ sources/install/package_base.sh
@@ -528,7 +528,7 @@ function package_base() {
     dos2unix ftp sshpass telnet nfs-common ncat netcat-traditional socat rdate putty \
     screen p7zip-full p7zip-rar unrar xz-utils xsltproc parallel tree ruby ruby-dev ruby-full bundler \
     nim perl libwww-perl openjdk-17-jdk \
-    logrotate tmux tldr bat libxml2-utils virtualenv chromium libsasl2-dev \
+    logrotate tmux bat libxml2-utils virtualenv chromium libsasl2-dev \
     libldap2-dev libssl-dev isc-dhcp-client sqlite3 dnsutils samba ssh snmp faketime php \
     python3 python3-dev grc emacs-nox xsel xxd libnss3-tools htop ripgrep pv
     apt-mark hold tzdata  # Prevent apt upgrade error when timezone sharing is enable
@@ -616,7 +616,7 @@ function package_base() {
     touch ~/.hushlogin
 
     # TLDR
-    mkdir -p ~/.local/share/tldr
+    pipx install --system-site-packages tldr
     tldr -u
 
     # NVM (install in context)
